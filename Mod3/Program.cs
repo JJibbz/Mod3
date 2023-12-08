97e73ee6750009c74f006dd0c896959e59c89c94
JJibbz <vkazancev322@gmail.com>
2023-12-09 01:19:53 +0700

    Добавлен код, с выполнением итогового задания модуля 3

--- Mod3/Program.cs
+++ Mod3/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Здравствуйте, пользователь! Как вас зовут?");
+            var name = Console.ReadLine();
+            Console.WriteLine("Введите ваш возраст");
+            var age = Console.ReadLine();
+            Console.WriteLine("Ваше имя {0}, а возраст {1}.", name, age);
+            Console.WriteLine("Введите дату вашего рождения");
+            var DateOfBirth = Console.ReadLine();
+            Console.WriteLine("Итак, вас зовут {0}, ваш возраст {1} и дата вашего рождения {2}.",name, age, DateOfBirth);
+            Console.ReadKey();
+            
         }
     }
 }
